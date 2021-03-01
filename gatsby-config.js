671ca8f3ac5654ad3e9ef555499783d8b59db252
Mearat Hou <mearatjames@gmail.com>
2021-03-01 16:11:58 +1100

    Connected to CONTENTFUL

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ const contentfulConfig = {
   accessToken:
     process.env.CONTENTFUL_ACCESS_TOKEN ||
     process.env.CONTENTFUL_DELIVERY_TOKEN,
+    downloadLocal: true,
 };
 
 // If you want to use the preview API please define
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => {
+  console.log(data)
+  return (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -17,6 +19,25 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
-)
+  )
+}
+
+
+export const query = graphql`
+  {
+    allContentfulContentType {
+      totalCount
+      nodes {
+        name
+      }
+    }
+    allContentfulServices {
+      nodes {
+        isVariant
+        title
+      }
+    }
+  }
+`
 
 export default IndexPage
